fd95e4d3102816ea28562d37f35ac3bc835eb913
Banana split <tboc300@gmail.com>
2022-07-08 01:09:35 +0000

    premier commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
             let hh = day.getHours() * 30;
             let mm = day.getMinutes() * 6;
             let ss = day.getSeconds() * 6;
-
+        
             hr.style.transform = `rotateZ(${hh+(mm/12)}deg)` ;
             mn.style.transform = `rotateZ(${mm}deg)`;
             sc.style.transform = `rotateZ(${ss}deg)`;
@@ -36,3 +36,4 @@
             seconds.innerHTML = s;
             ampm.innerHTML = am;
         })
+        
